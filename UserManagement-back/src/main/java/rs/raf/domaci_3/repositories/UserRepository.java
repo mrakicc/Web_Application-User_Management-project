8692a099a784045d79f317b8a6b030f8c1288029
mrakicc <92788079+mrakicc@users.noreply.github.com>
2024-09-27 14:51:26 +0200

    Add files via upload

---
+++ UserManagement-back/src/main/java/rs/raf/domaci_3/repositories/UserRepository.java
@@ -0,0 +1,13 @@
+package rs.raf.domaci_3.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import rs.raf.domaci_3.model.User;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+
+    User findByEmail(String email);
+
+}
